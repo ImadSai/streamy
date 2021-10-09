ad6f6409b65f2ebe81240cf27717d766e0d4b76f
imad <imad.salki@hotmail.fr>
2021-10-09 02:38:57 +0200

    First push

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
+
+class GoogleAuth extends React.Component {
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '312187773943-4gdh22v5qnjdrjm6amjhe5g5pnmdekf4.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.onAuthChange(this.auth.isSignedIn.get());
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            });
+        });
+
+    }
+
+    onAuthChange = (isSignedIn) => {
+        if (isSignedIn) {
+            this.props.signIn();
+        } else {
+            this.props.signOut();
+        }
+    }
+
+    onSigninClick = () => {
+        this.auth.signIn();
+    };
+
+    onSignoutClick = () => {
+        this.auth.signOut();
+    };
+
+    renderOfButton() {
+        if (this.props.isSignedIn === null) {
+            return null;
+        } else if (this.props.isSignedIn) {
+            return (<button className="ui red google button" onClick={this.onSignoutClick}>
+                <i className="google icon" />
+                Signout
+            </button>);
+        } else {
+            return (<button className="ui green google button" onClick={this.onSigninClick}>
+                <i className="google icon" />
+                Signin
+            </button>);
+        }
+    }
+
+    render() {
+        return <div> {this.renderOfButton()} </div>
+    }
+}
+
+// Mapping State to Props
+const mapStateToProps = (state) => {
+    return {
+        isSignedIn: state.authReducer.isSignedIn
+    }
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
---
+++ src/reducers/authReducer.js
@@ -0,0 +1,24 @@
+const INITIAL_STATE = {
+    isSignedIn: null
+};
+
+const authReducer = (state = INITIAL_STATE, action) => {
+
+    if (action.type === 'SIGN_IN') {
+        return {
+            ...state,
+            isSignedIn: true
+        }
+    }
+
+    if (action.type === 'SIGN_OUT') {
+        return {
+            ...state,
+            isSignedIn: false
+        }
+    }
+
+    return state;
+};
+
+export default authReducer;
